4716f662bca84a2abead78234344a13211ab9e2a
Mario <marioalbaladejomartinez@gmail.com>
2026-05-28 13:49:55 +0200

    primer commit

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+  public static void main(String[] args) {
+    Integer x = 111222211;
+    String y = x.toString();
+
+    StringBuilder compare = new StringBuilder();
+
+    for (int i = y.length() - 1; i >= 0; i--) {
+      compare.append(y.charAt(i));
+    }
+
+    if (compare.toString().equals(y)) {
+      System.out.println("True");
+
+    } else {
+      System.out.println("False");
+
+    }
+
+  }
+}
---
+++ Twosum.java
@@ -0,0 +1,24 @@
+public class Twosum {
+  public static void main(String[] args) {
+    int[] nums = { 3, 2, 4, 3 };
+    int target = 6;
+
+    boolean bool = false;
+    int i = 0;
+
+    while (bool == false && i < nums.length - 1) {
+
+      for (int j = i + 1; j < nums.length; j++) {
+
+        if (nums[i] + nums[j] == target) {
+          System.out.println("(" + i + ", " + j + ")");
+          bool = true;
+          break;
+        }
+
+      }
+      i++;
+    }
+
+  }
+}
